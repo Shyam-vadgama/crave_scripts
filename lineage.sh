8001402cce5bf3c413b6cb459b78109d0dd0652f
Shyam Vadgama <163844065+Shyam-vadgama@users.noreply.github.com>
2026-07-29 10:51:25 +0530

    Change DEVICE variable from 'X1' to 'warm'

--- lineage.sh
+++ lineage.sh
@@ -5,7 +5,7 @@
 # ==========================================
 START_TIME=$(date +%s)
 LOG_FILE="build_warm_$(date +%Y%m%d_%H%M).log"
-DEVICE="X1"
+DEVICE="warm"
 
 [ -f "$HOME/.bashrc" ] && source "$HOME/.bashrc"
 
