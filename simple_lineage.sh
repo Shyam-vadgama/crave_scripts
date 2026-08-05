ca66cdde1f1c5bed903999b712a1104df03f2694
Shyam Vadgama <163844065+Shyam-vadgama@users.noreply.github.com>
2026-08-05 22:10:36 +0530

    Initialize LineageOS build environment for warm

---
+++ simple_lineage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+rm -rf .repo/local_manifests
+
+# Repo init
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth=1
+
+# Build Sync
+/opt/crave/resync.sh
+
+rm -rf device/xiaomi/warm
+rm -rf vendor/xiaomi/warm
+rm -rf device/xiaomi/warm-kernel
+rm -rf hardware/xiaomi
+rm -rf hardware/qcom-caf/common
+
+# Device tree
+git clone https://github.com/Shyam-vadgama/device_xiaomi_warm -b lineage-23.2 device/xiaomi/warm
+
+# Kernel
+git clone https://github.com/Shyam-vadgama/warm_kernel -b main device/xiaomi/warm-kernel
+
+# Hardware
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi
+
+# Vendor tree
+git clone https://github.com/Shyam-vadgama/vendor_xiaomi_warm.git -b lineage-23.2 vendor/xiaomi/warm && cd vendor/xiaomi/warm && git checkout c0417ffc868d6f96f8e2ee6252eabbcebfa91927 && cd /crave-devspace/lineage
+
+git clone https://github.com/Shyam-vadgama/android_hardware_qcom-caf_common -b lineage-23.2 hardware/qcom-caf/common
+
+. build/envsetup.sh
+lunch lineage_warm-ap4a-userdebug || brunch warm
+mka bacon
