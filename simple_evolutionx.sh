ad7174bc3b71e1b464783f5a2653f1ab6a10b2f7
Shyam Vadgama <163844065+Shyam-vadgama@users.noreply.github.com>
2026-08-08 20:54:17 +0530

    bringup evolution x

---
+++ simple_evolutionx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Repo init
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs --depth=1
+
+# Build Sync
+/opt/crave/resync.sh
+
+rm -rf device/xiaomi/warm
+rm -rf vendor/xiaomi/warm
+rm -rf device/xiaomi/warm-kernel
+rm -rf hardware/xiaomi
+rm -rf hardware/qcom-caf/common
+
+# Device tree
+git clone https://github.com/Shyam-vadgama/device_xiaomi_warm -b evo device/xiaomi/warm
+
+# Kernel
+git clone https://github.com/Shyam-vadgama/warm-kernel -b main device/xiaomi/warm-kernel
+
+# Hardware
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi
+
+# Vendor tree
+git clone https://github.com/Shyam-vadgama/vendor_xiaomi_warm.git -b lineage-23.2 vendor/xiaomi/warm
+cd vendor/xiaomi/warm && git checkout c0417ffc868d6f96f8e2ee6252eabbcebfa91927 && cd -
+
+git clone https://github.com/Shyam-vadgama/android_hardware_qcom-caf_common -b lineage-23.2 hardware/qcom-caf/common
+
+. build/envsetup.sh
+lunch lineage_warm-ap4a-userdebug || lunch lineage_warm-bp4a-userdebug
+m evolution
